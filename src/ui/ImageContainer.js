1173d44c6f21946119d910bd4e8a00f7362fe112
Linschlager <linus4123@gmail.com>
2020-07-10 11:13:50 +0200

    Added fallback wallpaper

--- src/ui/ImageContainer.js
+++ src/ui/ImageContainer.js
@@ -4,7 +4,11 @@ const ImageContainer = ({ post }) => {
   return (
     <div
       className="image-post"
-      style={{ backgroundImage: `url(${post.url})` }}
+      style={{
+        backgroundImage: `url(${
+          post.url
+        }), url(https://images8.alphacoders.com/904/904400.jpg)`
+      }}
       title={post.title}
     />
   );
